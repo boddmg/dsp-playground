89d54cdb9e7bbed433041c4649220363ebc1abba
boddmg <boddmg@gmail.com>
2015-08-13 16:32:07 +0800

    Forget the FFT way to generate the time series.
    Signed-off-by: boddmg <boddmg@gmail.com>

--- experiment.py
+++ experiment.py
@@ -10,33 +10,64 @@ import thdn
 
 def single_frequency_filter(input_signal):
     y_f_all = np.fft.fft(input_signal)
-    y_f_all[:10] = np.array([0] *10)
+    y_f_all[:1] = np.array([0] *1)
     y_f_half = y_f_all[:len(y_f_all) / 2]
     y_f_abs = np.abs(y_f_half)
     y_f_max = max(y_f_abs)
     y_f_max_index = np.where(y_f_abs == y_f_max)[0][0]
+    print(y_f_max_index)
     y_f_all[:y_f_max_index] = [0] * (y_f_max_index)
     y_f_all[y_f_max_index+1:] = [0] * (len(y_f_all)-y_f_max_index-1)
     y_filtered = np.fft.ifft(y_f_all)
     return y_filtered
 
 
+FS = 204.8
+BASE_FREQUENCY = 50.0
+FILTER_SAMPLE_PURE = int(2*1/BASE_FREQUENCY * FS) # 2T
+FILTER_SAMPLE_ALL = 2048
+DF = FS/FILTER_SAMPLE_ALL
+print(DF)
+filter_buffer = []
+
+def single_filter(x):
+    if len(filter_buffer)<FILTER_SAMPLE_PURE:
+        filter_buffer.append(x)
+        return x
+    else:
+        filter_buffer.pop(0)
+        filter_buffer.append(x)
+        add_zero_data = np.array(filter_buffer + [0] * (FILTER_SAMPLE_ALL - FILTER_SAMPLE_PURE))
+        fft = np.fft.fft(add_zero_data)
+        fft_mag = np.abs(fft)
+        max_fft_mag = max(fft_mag[48.0/DF:52.0/DF])
+        max_fft_mag_index = np.where(fft_mag == max_fft_mag)[0][0]
+        phase = np.angle(fft[max_fft_mag_index])
+        print(phase)
+        return np.sin(np.pi * 2 * max_fft_mag_index * DF + phase) *500
+
+
+
+
 def main():
+    import json
     # get data
-    meta_data = pickle.load(open("meta_data.pkl", "rb"))
+    data = json.load(open("data.txt", "r"))
     # y = np.concatenate((np.load("data.pkl")[:256], np.array([0] * (600 - 256))))
     # y = np.concatenate((np.load("data.pkl")[:], [0]*6000))
-    y = np.load("data.pkl")[:200]
-    y = signal.resample(y, 5000)
+    y = np.array(data["Y"])
+    # y = signal.resample(y, 5000)
 
-    fs = 5000.0 * (10000/200)
+    # fs = 5000.0 * (10000/200)
+    fs = 1 / data["dt"]
+    print("fs:\t", fs)
     time = len(y)/fs # in seconds
     x = np.arange(0, time, 1/fs)
     # x = x[:-1]
-    for i in meta_data:
-        print(i, meta_data[i])
+    # for i in meta_data:
+    #     print(i, meta_data[i])
     print("time",time)
-    end = 1000
+    end = 40
     f = x[:end]
     f = f * fs / time
 
@@ -49,7 +80,12 @@ def main():
     plt.plot(x, y, 'r')
 
     plt.subplot(232)
-    y_filtered = single_frequency_filter(y)*10
+    y_filtered = y.tolist()
+    y_list = y.tolist()
+    for i in range(len(y_list)):
+        y_filtered[i] = single_filter(y_list[i])
+    y_filtered = np.array(y_filtered)
+    # y_filtered = single_frequency_filter(y)*10
     # filter_function = np.array(([0] * 6 + [1] + [0] * (600 - 7)))
     # filter_function = np.fft.ifft(filter_function)
     # y_filtered = np.fft.ifft(y_f * filter_function)
